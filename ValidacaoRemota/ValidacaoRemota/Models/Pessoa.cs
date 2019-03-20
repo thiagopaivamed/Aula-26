50f8002cad7862b02f17a2967013e7b38a7cc389
Thiago Paiva  Medeiros <thiagopaivamed@gmail.com>
2019-03-19 21:46:41 -0300

    Validação Remota

---
+++ ValidacaoRemota/ValidacaoRemota/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidacaoRemota.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+
+        [Remote("UsuarioExisteAsync", "Pessoas")]
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
